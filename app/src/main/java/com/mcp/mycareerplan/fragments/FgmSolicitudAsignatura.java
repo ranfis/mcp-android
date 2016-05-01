f0439aef4c3496a6be9d79fc70b2f3e4fd359659
Ranfis S <ranfis.sh@gmail.com>
2016-04-30 20:25:34 -0400

    WIP: Publish subject application form.

---
+++ app/src/main/java/com/mcp/mycareerplan/fragments/FgmSolicitudAsignatura.java
@@ -0,0 +1,51 @@
+package com.mcp.mycareerplan.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mcp.mycareerplan.R;
+
+public class FgmSolicitudAsignatura extends Fragment  {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FgmSolicitudAsignatura() {
+        // Required empty public constructor
+    }
+
+    public static FgmSolicitudAsignatura newInstance() {
+        FgmSolicitudAsignatura fragment = new FgmSolicitudAsignatura();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getActivity().getResources().getString(R.string.title_fragment_indice));
+
+        View view = inflater.inflate(R.layout.fragment_solicitud_asignatura,container, false);
+        return view;
+    }
+
+}
