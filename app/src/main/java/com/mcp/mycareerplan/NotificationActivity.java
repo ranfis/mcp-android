0ba51fab822c015907f03ad29c41405b1636aa48
Luis Martinez <luizmartiinez@gmail.com>
2016-05-06 23:19:00 -0400

    Fix some issues with notification

--- app/src/main/java/com/mcp/mycareerplan/NotificationActivity.java
+++ app/src/main/java/com/mcp/mycareerplan/NotificationActivity.java
@@ -2,12 +2,94 @@ package com.mcp.mycareerplan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mcp.mycareerplan.api.university.Universidad;
+import com.mcp.mycareerplan.models.UniversidadMock;
+import com.squareup.picasso.Picasso;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    ImageView ivImageContent;
+    TextView tvTitle;
+    TextView tvContent;
+    ImageView ivImageUniversity;
+    TextView tvNameUniversity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        ivImageContent = (ImageView) findViewById(R.id.notification_big_image);
+        tvTitle = (TextView) findViewById(R.id.notification_title);
+        tvContent = (TextView) findViewById(R.id.notification_content);
+        ivImageUniversity = (ImageView) findViewById(R.id.notification_university_logo);
+        tvNameUniversity = (TextView) findViewById(R.id.notification_university_name);
+
+        String urlContenido = "";
+        int universidad = 0;
+        String titulo = "";
+        String contenido = "";
+
+        if(savedInstanceState == null){
+            Bundle subjectIfo = getIntent().getExtras();
+            if(subjectIfo == null){
+                urlContenido = "urlContenido";
+                universidad = 0;
+                titulo = "titulo";
+                contenido = "contenido";
+            }
+            else{
+                urlContenido = subjectIfo.getString("urlContenido"); // Codigo de la asignatura
+                universidad = Integer.parseInt(subjectIfo.getString("universidad")); // Nombre de la asignatura
+                titulo = subjectIfo.getString("titulo"); // Nombre de la asignatura
+                contenido = subjectIfo.getString("contenido"); // Nombre de la asignatura
+            }
+        }
+        else{
+            urlContenido = (String) savedInstanceState.getSerializable("urlContenido");
+            universidad = (int) savedInstanceState.getSerializable("universidad");
+            titulo = (String) savedInstanceState.getSerializable("titulo");
+            contenido = (String) savedInstanceState.getSerializable("contenido");
+        }
+
+        tvTitle.setText(titulo);
+        tvContent.setText(Html.fromHtml(contenido));
+        UniversidadMock u = universitySelection(universidad);
+        tvNameUniversity.setText(u.getName());
+
+
+        Picasso
+                .with(this)
+                .load(u.getUrlImage())
+                .fit()
+                .centerInside()
+                .into(ivImageUniversity);
+
+        Picasso
+                .with(this)
+                .load(urlContenido)
+                .fit()
+                .centerCrop()
+                .into(ivImageContent);
+
+    }
+
+    private UniversidadMock universitySelection(int idUniversity) {
+        UniversidadMock uni = new UniversidadMock();
+        if(idUniversity == 1) {
+            uni.setName("Universidad Iberoamericana");
+            uni.setId(1);
+            uni.setUrlImage(R.drawable.unibe);
+        } else if(idUniversity==2) {
+            uni.setName("Universidad APEC");
+            uni.setId(2);
+            uni.setUrlImage(R.drawable.logo_apec);
+        }
+
+        return uni;
     }
 }
--- app/src/main/java/com/mcp/mycareerplan/models/UniversidadMock.java
+++ app/src/main/java/com/mcp/mycareerplan/models/UniversidadMock.java
@@ -4,4 +4,40 @@ package com.mcp.mycareerplan.models;
  * Created by LuisMartinez on 05/04/16.
  */
 public class UniversidadMock {
+    int id;
+    String name;
+    int urlImage;
+    String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(int urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
